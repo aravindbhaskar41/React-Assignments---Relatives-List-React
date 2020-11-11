568138516e8e653281d0f4ea59d49443b08af050
aravindbhaskar41 <aravindbhaskar41@gmail.com>
2020-11-12 01:45:10 +0530

    Test1

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <ol key="relativeList">
+          <li key="relativeListItem1">Batman</li>
+          <li key="relativeListItem2">Superman</li>
+          <li key="relativeListItem3">Heman</li>
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
